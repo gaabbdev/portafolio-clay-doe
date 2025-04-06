ee33dd58127653903a1b2e857d2453042d43f681
Gabriel <gabbdev52@proton.me>
2025-04-06 17:44:50 -0400

    Portafolio clay doe finalizado

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+const bar = document.querySelector('#bar');
+const nav = document.querySelector('#menu');
+const menu = document.querySelector('.menu');
+
+eventos();
+
+function eventos() {
+  bar.addEventListener('click', () => nav.classList.toggle('menu-elementos--show'));
+  
+  document.addEventListener('scroll', () => window.scrollY > 560 ? menu.classList.add("menu-bg") : menu.classList.remove("menu-bg"));
+}
