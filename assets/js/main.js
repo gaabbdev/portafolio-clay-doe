2816e132494d2b0eb70228bcdd0bcf8b60a8c4e7
Gabriel <gabbdev52@proton.me>
2025-04-06 18:03:36 -0400

    Proyecto refactorizado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,17 @@
-const bar = document.querySelector('#bar');
-const nav = document.querySelector('#menu');
-const menu = document.querySelector('.menu');
+const bar = document.querySelector("#bar");
+const nav = document.querySelector("#menu");
+const menu = document.querySelector(".menu");
 
 eventos();
 
 function eventos() {
-  bar.addEventListener('click', () => nav.classList.toggle('menu-elementos--show'));
-  
-  document.addEventListener('scroll', () => window.scrollY > 560 ? menu.classList.add("menu-bg") : menu.classList.remove("menu-bg"));
+  bar.addEventListener("click", () =>
+    nav.classList.toggle("menu-elementos--show")
+  );
+
+  document.addEventListener("scroll", () =>
+    window.scrollY > 560
+      ? menu.classList.add("menu-bg")
+      : menu.classList.remove("menu-bg")
+  );
 }
